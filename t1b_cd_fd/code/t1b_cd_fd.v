cffb4ca157faba6f5080327de3ebbce684caf60b
Eyantra <vgpsreeram@gmail.com>
2024-10-13 14:56:16 +0530

    Updated task1b

--- t1b_cd_fd/code/t1b_cd_fd.v
+++ t1b_cd_fd/code/t1b_cd_fd.v
@@ -29,15 +29,50 @@ module t1b_cd_fd (
 //////////////////DO NOT MAKE ANY CHANGES ABOVE THIS LINE //////////////////
 
 reg [8:0] counter = 0;
-reg [14:0] cs_counter, csf_counter = 0;
-reg [1:0] pcolor; // temporary register to store previous color.
+reg [5:0] frequency = 0;
+reg [5:0] prev_frequency = 0;
+reg [1:0] prev_color = 0;
+
+parameter RED_FILTER = 0, BLUE_FILTER = 1, CLEAR_FILTER = 2, GREEN_FILTER = 3;
+parameter RED_COLOR = 1, BLUE_COLOR = 3, CLEAR_COLOR = 0, GREEN_COLOR = 2;  
 
 initial begin // editing this initial block is allowed
     
-	 filter = 3; 
-	 color = 0;
-	 pcolor = 0;
+	 filter = GREEN_FILTER; 
+	 color = CLEAR_COLOR;
 	 
+end
+
+always @(posedge cs_out) begin
+	
+	if (counter == 100) begin
+	
+		frequency = frequency + 1;
+	
+	end
+	
+	if (counter == 500) begin
+	
+		if (prev_frequency < frequency) begin
+		
+			case (filter)
+		
+				RED_FILTER: prev_color = RED_COLOR;
+				BLUE_FILTER: prev_color = BLUE_COLOR;
+				GREEN_FILTER: prev_color = GREEN_COLOR;
+		
+			endcase
+			
+			prev_frequency = frequency;
+			
+		end
+		
+		frequency = 0;
+	
+	end
+	
+	if (filter == CLEAR_FILTER) prev_frequency = 0;
+
 end
 
 always @(posedge clk_1MHz) begin
@@ -45,69 +80,28 @@ always @(posedge clk_1MHz) begin
 	if (counter == 500) begin
 	
 		case (filter)
-			
-			3: begin
-				filter = 0;
+		
+			RED_FILTER: filter = BLUE_FILTER;
+			BLUE_FILTER: begin
+				filter = CLEAR_FILTER;
+				color = prev_color;
 			end
-				
-			0: begin
-				filter = 1;
-			end
-				
-			1: begin
-				color = pcolor;
-				filter = 2;
-			end
-				
-			default;
-			
+			GREEN_FILTER: filter = RED_FILTER;
+		
 		endcase
 		
 		counter = 0;
-		
+	
 	end
 	
-	if (filter == 2 && counter == 1) begin
-		
-		filter = 3;
+	if (counter == 1 && filter == CLEAR_FILTER) begin
+	
+		filter = GREEN_FILTER;
 		counter = 0;
-
+	
 	end
-		
+	
 	counter = counter + 1;
-
-end
-
-always @(posedge cs_out) begin
-
-	if (counter == 500) begin
-	
-		if (csf_counter < cs_counter) begin
-		
-			csf_counter = cs_counter;
-			
-			case (filter)
-		
-				0: pcolor = 1;
-				1: pcolor = 3;
-				3: pcolor = 2;
-				default;
-		
-			endcase
-		
-		end
-		
-		cs_counter = 0;
-	
-	end
-	
-	if (filter == 2) begin
-	
-		csf_counter = 0;
-	
-	end
-	
-	cs_counter = cs_counter + 1;
 
 end
 
